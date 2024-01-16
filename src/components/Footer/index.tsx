23cc21d022b0e4087922a5746419a5663e0d86a5
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-01-16 16:20:04 -0300

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+export default function Footer(){
+
+    return(
+        <footer>
+            <div className="ct-container ct-footer-city-insta">
+                <div><i>@dscarrostop</i></div>
+                <div><i>Rua da Pipoca, 150</i></div>
+            </div>
+        </footer>
+    );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,20 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+export default function Catalog(){
+
+    return (
+        <>
+            <Header />
+                <main>
+                    <section>
+                        Teste
+                    </section>
+                    <section>
+                        Teste
+                    </section>
+                </main>
+            <Footer />
+        </>
+    );
+}
