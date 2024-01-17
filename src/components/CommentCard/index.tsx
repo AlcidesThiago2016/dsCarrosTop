c33f7d2cc20a68947c7ed0953bfdc4c51e93d276
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-01-17 09:20:28 -0300

    Create commentCard

---
+++ src/components/CommentCard/index.tsx
@@ -0,0 +1,29 @@
+import './styles.css';
+
+
+export default function CommentCard(){
+
+    return(
+        <div className="comment-card">
+            <h6>O que estão dizendo</h6>
+            <div className="comment-top-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+            <div className="comment-top-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+            <div className="comment-top-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+            <div className="comment-top-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+            <div className="comment-top-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+            <div className="comment-top-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+            </div>
+        </div>
+    );
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import CardCar from "../../components/CardCar";
+import CommentCard from "../../components/CommentCard";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
@@ -18,6 +19,7 @@ export default function Catalog(){
                        </div>
                        </section>
                 </main>
+                <CommentCard />
             <Footer />
         </>
     );
